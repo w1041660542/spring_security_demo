3be406e0acad414fe3bc6c985a2f1452b6fce38c
w1041660542 <why199627@gmail.com>
2020-07-26 10:25:33 +0800

    添加注释测试

--- apacheshiro/src/main/java/com/mmall/demo2/mapper/UserMapper.java
+++ apacheshiro/src/main/java/com/mmall/demo2/mapper/UserMapper.java
@@ -4,6 +4,10 @@ import com.mmall.demo2.model.User;
 import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
-
+    /**
+     * 通过用户名查询用户
+     * @param username
+     * @return
+     */
     User findByUsername(@Param("username") String username);
 }
